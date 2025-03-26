84e8176477fb8b09e008a3ccf53d99e2c5bd7d96
ICP.Ninja <noreply@icp.ninja>
2025-03-26 03:13:57 +0100

    ICP.Ninja export

---
+++ frontend/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import '../index.css';
+import CeloL2Dashboard from './CeloL2Dashboard';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CeloL2Dashboard />
+  </React.StrictMode>
+);
